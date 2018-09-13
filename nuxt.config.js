c2020eb7e3d8173da8677cf3b648bcaeabb1d85d
sKawashima <bysk.333@gmail.com>
2018-09-14 03:14:35 +0900

    fix router

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,17 +59,17 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend (config, ctx) {
-      const urlLoader = config.module.rules.find((rule) => rule.loader === 'url-loader')
-      urlLoader.test = /\.(png|jpe?g|gif|svg)$/
-      config.module.rules.push({
-        enforce: 'pre',
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        loader: 'url-loader',
-        query: {
-          limit: 1000000, // 1MB
-          name: './[name].[hash:7].[ext]'
-        }
-      })
+      // const urlLoader = config.module.rules.find((rule) => rule.loader === 'url-loader')
+      // urlLoader.test = /\.(png|jpe?g|gif|svg)$/
+      // config.module.rules.push({
+      //   enforce: 'pre',
+      //   test: /\.(png|jpe?g|gif|svg)$/i,
+      //   loader: 'url-loader',
+      //   query: {
+      //     limit: 1000000, // 1MB
+      //     name: './[name].[hash:7].[ext]'
+      //   }
+      // })
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
@@ -81,6 +81,5 @@ module.exports = {
       }
     }
   },
-  generate: { dir: 'docs' },
-  router: { base: './' }
+  generate: { dir: 'docs' }
 }
