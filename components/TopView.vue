f938684e8d531a22e0a63290133b440da7c22c4c
ryu-g <em.ryu.g@gmail.com>
2018-09-14 04:03:58 +0900

    insert loooogooooo XD

--- components/TopView.vue
+++ components/TopView.vue
@@ -1,11 +1,17 @@
 <template lang='pug'>
 #topView
+  <img src="~/static/motionlogo.gif" alt="">
 </template>
-
 <script>
 export default {
 }
 </script>
 
 <style lang='sass' scoped>
+#topView
+  display: flex
+  align-items: center
+  justify-content: center
+  height: 100vh
+  background-color: #03635B
 </style>
