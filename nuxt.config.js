f82a2bdefee6074a21f3c4657dbad39ea16f20b8
sKawashima <bysk.333@gmail.com>
2018-09-14 05:11:18 +0900

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,11 +11,10 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description },
-      { name: 'robots', content: 'noindex' }
+      { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/web/favicon.ico' }
     ]
   },
 
