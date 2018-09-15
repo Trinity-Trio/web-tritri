e480e4a0b44fd6a96bd420de1246b370ed08e150
ryu-g <em.ryu.g@gmail.com>
2018-09-16 04:16:54 +0900

    スクロールガイド(仮)を設置してみた

--- components/TopView.vue
+++ components/TopView.vue
@@ -1,6 +1,7 @@
 <template lang='pug'>
 #topView
   img.logo(src='~/static/motionlogo.gif')
+  img.guide(src='~/assets/loading.svg')
 </template>
 <script>
 export default {
@@ -10,11 +11,23 @@ export default {
 <style lang='sass' scoped>
 #topView
   display: flex
+  flex-direction: column
   align-items: center
   justify-content: center
   height: 100vh
   background-color: #03635B
-  img
+  img.logo
     max-width: 80%
-
+  img.guide
+    width: 80px
+    height: 80px
+    animation: rotation 1.55s infinite ease
+    animation-delay: 2s
+    @keyframes rotation
+      0%
+        transform: rotate(-60deg)
+      50%
+        transform: rotate(180deg)
+      100%
+        transform: rotate(180deg)
 </style>
